d3070254c383d36e037567b7934a305d8e247f5c
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-24 21:47:48 +0900

    113. Hive 사용 준비

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todolist/model/todo.dart';
 import 'package:todolist/presentation/list_screen.dart';
 
-void main() {
+void main() async {
+
+
+  // 탑레벨
+  late final  Box<Todo> todos;
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  // 로드 하고
+ Box<Todo> todosDB =await Hive.openBox<Todo>('todoList.db');
   runApp(const MyApp());
 }
 
--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,14 +1,19 @@
-class Todo {
+import 'package:hive/hive.dart';
+
+part 'todo.g.dart';
+
+
+@HiveType( typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
   String title; //제목
+  @HiveField(2)
   int dateTime; // 시간
 
   Todo({
     required this.title,
     required this.dateTime,
   });
-
-
-  
-
-
 }
