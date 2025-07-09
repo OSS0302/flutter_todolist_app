db02da4a5c52049703c3e1e3e64a19f383695f8f
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2025-07-09 10:56:41 +0900

    기능 추가

--- lib/presentation/list_screen.dart
+++ lib/presentation/list_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:todolist/main.dart';
 import 'package:todolist/presentation/add_screen.dart';
 import 'package:todolist/presentation/todo_item.dart';
@@ -33,46 +32,56 @@ class _ListScreenState extends State<ListScreen> {
             children: todos.values
                 .map(
                   (todoE) => TodoItem(
-                    todo: todoE,
-                    onTapCallBack: (todo) async {
-                      todo.isDone = !todo.isDone;
-                      await todo.save();
-
-                      // 저장하고  화면갱신
-                      setState(() {});
-                    },
-                    onDelete: (Todo) async {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('삭제할까요?'),
-                          content: const Text('삭제 이후엔 복구할 수 없습니다.'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text(
-                                '취소',
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () async {
-                                 todoE.delete();
-                                 Navigator.of(context).pop();
-                                //지우고 화면갱신
-                                setState(() {});
-                              },
-                              child: Text(
-                                '예',
-                              ),
-                            ),
-                          ],
+                todo: todoE,
+                onTapCallBack: (todo) async {
+                  todo.isDone = !todo.isDone;
+                  await todo.save();
+                  setState(() {});
+                },
+                onDelete: (todo) async {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('삭제할까요?'),
+                      content: const Text('삭제 이후엔 복구할 수 없습니다.'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('취소'),
                         ),
-                      );
-                    },
-                  ),
-                )
+                        TextButton(
+                          onPressed: () async {
+                            await todo.delete();
+                            Navigator.of(context).pop();
+
+                            await showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text('삭제되었습니다'),
+                                content: const Text('다음 일정을 추가 해 주세요'),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('확인'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop(); // 이 다이얼로그 닫기
+                                    },
+                                  ),
+                                ],
+                              ),
+                            );
+
+                            setState(() {});
+                          },
+                          child: const Text('예'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
                 .toList(),
           ),
         ],
@@ -84,7 +93,6 @@ class _ListScreenState extends State<ListScreen> {
             context,
             MaterialPageRoute(builder: (context) => const AddScreen()),
           );
-          // 화면 갱신
           setState(() {});
         },
         child: const Icon(Icons.add),
